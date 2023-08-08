8cca9bd31ce776b22f332a0ad4ad5adb3180473b
Mr-Mind-Freak <navaneethan2482@gmail.com>
2023-08-08 22:13:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Restaurent
-Simple restaurent website with plain html css and vannila js
+
+This website is made by me after learning ui design on Figma for testing my designing knowledge. Currently, Only the desktop version is available and other versions will be added soon.
+
+The prize of items and total amount gets changed using the 'focusout' and 'blur' event listeners in vanilla js
+
+###[Restaurent design](https://www.figma.com/file/V2IqF7oFRIJ4sAFIBStSZl/Restaurent?type=design&node-id=0%3A1&mode=design&t=X8mwhjZKNpsC7nok-1)
---
+++ app.js
@@ -0,0 +1,61 @@
+const pizzaQ = document.getElementById('pizzaQ');
+const burgerQ = document.getElementById('burgerQ');
+const breadQ = document.getElementById('breadQ');
+const chickenQ = document.getElementById('chickenQ');
+const pizzaAmount = document.getElementById('pizzaAmount');
+const burgerAmount = document.getElementById('burgerAmount');
+const breadAmount = document.getElementById('breadAmount');
+const chickenAmount = document.getElementById('chickenAmount');
+const total = document.getElementById('totalAmount');
+
+const setFocus = (inputBox) => {
+    inputBox.focus();
+}
+
+const changeAmount = (rate,inputBox,amount) => {
+    let quantity = inputBox.value;
+    if (quantity > 0){
+        let cost = amount.textContent;
+        cost = cost.slice(1);
+        cost = parseFloat(cost);
+        cost = rate * quantity;
+        amount.textContent = `$${cost}`;
+        let tot = parseFloat(total.textContent.slice(1));
+        tot += cost;
+        total.textContent = `$${tot.toFixed(2)}`;
+    }
+    else{
+        window.alert('Quantity should be greater than 0');
+        inputBox.value = '';
+    }
+}
+
+const reset = () => {
+    pizzaQ.value = '';
+    burgerQ.value = '';
+    chickenQ.value = '';
+    breadQ.value = '';
+    pizzaAmount.textContent = '$0.00';
+    burgerAmount.textContent = '$0.00';
+    breadAmount.textContent = '$0.00';
+    chickenAmount.textContent = '$0.00';
+    total.textContent = '$0.00'
+}
+
+document.getElementById('pizza-pic').addEventListener('click',()=>{ setFocus(pizzaQ) });
+document.getElementById('burger-pic').addEventListener('click',()=>{ setFocus(burgerQ) });
+document.getElementById('cornbread-pic').addEventListener('click',()=>{ setFocus(breadQ) });
+document.getElementById('hot_chicken-pic').addEventListener('click',()=>{ setFocus(chickenQ) });
+
+pizzaQ.addEventListener('blur', (e) => {
+    changeAmount(12.55,e.target,pizzaAmount);
+});
+burgerQ.addEventListener('focusout',(e) => {
+    changeAmount(15.33,e.target,burgerAmount);
+});
+breadQ.addEventListener('blur', (e) => {
+    changeAmount(8.88,e.target,breadAmount);
+});
+chickenQ.addEventListener('focusout',(e) => {
+    changeAmount(10.00,e.target,chickenAmount);
+});
